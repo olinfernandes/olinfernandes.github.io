5ca779fe94214ec62044cd4b7a234ccf8d90707a
olinfernandes <olin.f@live.com>
2024-07-13 21:32:43 +0530

    Adds deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://olinfernandes.github.io',
+  base: 'blogs',
 	integrations: [mdx(), sitemap()],
 });
