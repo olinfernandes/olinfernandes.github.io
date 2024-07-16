0fbd7b268f6d5bd43c8d01773c1d63d52bfbf4cf
olinfernandes <olin.f@live.com>
2024-07-16 21:51:30 +0530

    Removes Xata Integration

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-interface ImportMetaEnv {
-  readonly XATA_API_KEY: string;
-  readonly XATA_BRANCH?: string;
-  readonly XATA_DB_URL: string;
-}
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
