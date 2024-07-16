8b9d190d7238daff5507b8bbc8328c2f61eedd47
olinfernandes <olin.f@live.com>
2024-07-16 20:52:08 +0530

    Reset and Squashed

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly XATA_API_KEY: string;
+  readonly XATA_BRANCH?: string;
+  readonly XATA_DB_URL: string;
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
