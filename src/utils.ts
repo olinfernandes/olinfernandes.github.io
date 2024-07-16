15d555774cb3d04d431db0640cb28ab6614cc45b
olinfernandes <olin.f@live.com>
2024-07-16 21:51:30 +0530

    Removes Xata Integration

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,5 @@
-import { XataClient } from './xata';
-
 export const calcReadTime = (body: string) => {
   const averageSpeed = 200;
   const wordCount = body.split(' ').length;
   return Math.ceil(wordCount / averageSpeed);;
 }
-
-export const xata = new XataClient({
-  databaseURL: import.meta.env.XATA_DB_URL,
-  apiKey: import.meta.env.XATA_API_KEY,
-  branch: import.meta.env.XATA_BRANCH,
-});
