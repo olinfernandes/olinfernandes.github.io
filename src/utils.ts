867fb017f10e5960398477fe58f5788d8139e3c5
olinfernandes <olin.f@live.com>
2024-07-19 20:13:59 +0530

    Update - Adds new blog The Voice Within

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 export const calcReadTime = (body: string) => {
-  const averageSpeed = 200;
-  const wordCount = body.split(' ').length;
-  return Math.ceil(wordCount / averageSpeed);;
-}
+  const averageWpm = 200;
+  const wordCount = body.split(" ").length;
+  return Math.ceil(wordCount / averageWpm);
+};
