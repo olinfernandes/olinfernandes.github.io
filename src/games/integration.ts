477a4e7c12ac7b9500031b9bd1cd50bec15136be
olinfernandes <olin.f@live.com>
2025-04-03 11:34:46 +0530

    up

--- src/games/integration.ts
+++ src/games/integration.ts
@@ -6,7 +6,7 @@ export default function () {
     hooks: {
       "astro:config:setup": ({ injectRoute }) => {
         injectRoute({
-          pattern: "/games/*",
+          pattern: "/games/",
           entrypoint: "./src/games/index.astro",
           prerender: true,
         });
